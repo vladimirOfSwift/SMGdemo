06940c53b23657aedbeb59dcb3ec5d0cc0849efa
vladimirOfSwift <vladimirsavic456@gmail.com>
2025-06-05 14:27:42 +0200

    Initial Commit

---
+++ SMGdemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SMGdemo
+//
+//  Created by Vladimir Savic on 5. 6. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SMGdemo/SMGdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMGdemoApp.swift
+//  SMGdemo
+//
+//  Created by Vladimir Savic on 5. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct SMGdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
